9ba9ede323b7c946eb54b9b27886ef48ce60ce23
Carlosdpastrana <carlosdpastrana@icloud.com>
2025-02-18 22:02:54 -0700

    Added pictures
    Added pictures and fixed code for deployment

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,11 +1,11 @@
 function Project({ title, image, deployedLink, repoLink }) {
     return (
       <div>
-        <h3>{title}</h3>
-        <img src="/project1.png" alt="Project 1" />
+        <h3>Taskflow</h3>
+        <img src="/project1.jpg" alt="Project 1" />
         <div>
           <a href={deployedLink} target="_blank" rel="noopener noreferrer">Live App</a>
-          <a href={repoLink} target="_blank" rel="noopener noreferrer">GitHub Repo</a>
+          <a href="https://github.com/anton10mata/TaskFlow.git" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
         </div>
       </div>
     );
